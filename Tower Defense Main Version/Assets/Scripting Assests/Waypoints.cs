8d40a28c0ecd463a64e0527b0c5139fc9e96dfa1
Declan Holmes-Carr <29772947+Enoai@users.noreply.github.com>
2017-09-22 20:40:28 +0100

    Reverted back to orignal.

--- Tower Defense Main Version/Assets/Scripting Assests/Waypoints.cs
+++ Tower Defense Main Version/Assets/Scripting Assests/Waypoints.cs
@@ -6,39 +6,15 @@ public class Waypoints : MonoBehaviour {
     // make a duplicate of everything in here and make ai nt so they have to cohose between 2 ways
 
     public static Transform[] points; // Static variable holding all Waypoitns
-    public static Transform[] points1; // Static variable holding all Waypoitns
-
-    public int WaypointID;
-
-    void Start()
-    {
-
-    }
 
     void Awake() // Find all waypoints and load into point array
     {
-        if (WaypointID == 1)
+        points = new Transform[transform.childCount]; // creates ("X") amounts of spaces in the points array (bases on how many waypoints)
+
+        for (int i = 0; i < points.Length; i++) // Goes through them all!
         {
-            points = new Transform[transform.childCount]; // creates ("X") amounts of spaces in the points array (bases on how many waypoints)
-
-            for (int i = 0; i < points.Length; i++) // Goes through them all!
-            {
-                points[i] = transform.GetChild(i); // saves their location
-                Debug.Log(i);
-            }
+            points[i] = transform.GetChild(i); // saves their location
         }
-
-        if (WaypointID == 2)
-        {
-            points1 = new Transform[transform.childCount]; // creates ("X") amounts of spaces in the points array (bases on how many waypoints)
-
-            for (int i = 0; i < points1.Length; i++) // Goes through them all!
-            {
-                points1[i] = transform.GetChild(i); // saves their location
-                Debug.Log(i + "!");
-            }
-        }
-
     }
 
 }
